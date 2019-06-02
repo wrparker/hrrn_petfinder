38738e4cb368b88bd517a031cd8bcb5c9ab23d13
rparker <rparker@indeed.com>
2019-06-02 18:15:15 -0500

    Plugin skeleton

---
+++ hrrn_petfinder/hrrn_petfinder.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: HRRN Petfinder
+ * Plugin URI: https://github.com/wrparker/hrrn_petfinder
+ * Description: Use Petfinder API to display rabbits.
+ * Version: 0.1
+ * Author: W. Ryan Parker
+ * Author URI: https://wrparker.me
+ */
